13eb7413139722ab154b9249e369a9fb25068f55
es9d <larastar721@gmail.com>
2020-03-17 13:04:47 +0700

    update - airlock logout method

--- app/Http/Controllers/APIs/Auth/AuthController.php
+++ app/Http/Controllers/APIs/Auth/AuthController.php
@@ -14,14 +14,15 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'register', 'register_verify', 'lost_password', 'lost_password_verify', 'password_renew']]);
+        $this->middleware('auth:airlock', ['except' => ['login', 'register', 'register_verify', 'lost_password', 'lost_password_verify', 'password_renew']]);
     }
 
     public function login()
     {
         $validator = Validator::make(request()->all(), [
             'email' => 'required|string|email',
-            'password' => 'required|string'
+            'password' => 'required|string',
+            'device' => 'nullable|string|alpha_num'
         ]);
         if ($validator->fails()) {
             return response()->json(errorResponse($validator->errors()), 202);
@@ -31,9 +32,17 @@ class AuthController extends Controller
             return response()->json(errorResponse('Account not found !'), 202);
         }
         if (Auth::user()->active == User_setActiveStatus('active')) {
-            return $this->respondWithToken(Auth::user()->createToken('_jwtApiToken')->plainTextToken);
+            return $this->respondWithToken(Auth::user()->createToken(request('device') ? (request('device') . "-" . getClientIpAddress()) : ("_jwtApiToken-" . getClientIpAddress()))->plainTextToken);
         }
         return response()->json(errorResponse('Your account has been ' . User_getActiveStatus(Auth::user()->active)), 202);
+    }
+
+    public function logout()
+    {
+        if (Auth::user()->tokens()->delete()) {
+            return response()->json(successResponse('Successfully Logout'), 201);
+        }
+        return response()->json(errorResponse('Failed to Logout'), 202);
     }
 
     protected function respondWithToken($token)
