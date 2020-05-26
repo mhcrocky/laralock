6e0bc797cffaec3e06940a3f978130928c6d7a9a
es9d <larastar721@gmail.com>
2020-05-26 11:48:32 +0700

    update - admin menu new members

--- app/Http/Controllers/APIs/Admin/AdminMenuController.php
+++ app/Http/Controllers/APIs/Admin/AdminMenuController.php
@@ -22,23 +22,41 @@ class AdminMenuController extends Controller
         ]);
         if ($validator->fails()) return response()->json(errorResponse($validator->errors()), 202);
         $data = [];
+        /**
+         * get user count and list
+         */
         if (request()->has('_users')) {
-            $getUser = DB::table('users')->join('user_biodatas', 'users.code', '=', 'user_biodatas.code')->join('user_statuses', 'users.code', '=', 'user_statuses.code')->select('name', 'users.code', 'profile_img', 'active', 'status', 'users.created_at')->where('user_statuses.status', User_setStatus('user'));
+            $getUsers = $this->getUsers()->where('email_verified_at', '!=', null);
             if (request('_users') == 'countOnly') {
-                $data['users'] = strval($getUser->count());
+                $data['users'] = strval($getUsers->count());
             } else {
-                $data['users']['count'] = strval($getUser->count());
-                $data['users']['list'] = $getUser->get()->map(function ($user) {
+                $data['users']['count'] = strval($getUsers->count());
+                $data['users']['list'] = $getUsers->get()->map(function ($user) {
+                    $lastLogin = count($this->getUserHistory($user->code)->get()) ? Carbon_HumanDateTime($this->getUserHistory($user->code)->latest()->first()->created_at) : 'Never Logged In';
+                    return ['name' => $user->name, 'profile_img' => $user->profile_img, 'code' => $user->code, 'active' => ucfirst(User_getActiveStatus($user->active)), 'last_login' => $lastLogin, 'registered' => Carbon_HumanDateTime($user->created_at)];
+                });
+            }
+        }
+        if (request()->has('_newMembers')) {
+            $getNewMembers = $this->getUsers()->where('email_verified_at', null);
+            if (request('_newMembers') == 'countOnly') {
+                $data['newMembers'] = strval($getNewMembers->count());
+            } else {
+                $data['newMembers']['count'] = strval($getNewMembers->count());
+                $data['newMembers']['list'] = $getNewMembers->get()->map(function ($user) {
                     return ['name' => $user->name, 'profile_img' => $user->profile_img, 'status' => User_getStatusForHuman($user->status), 'code' => $user->code, 'active' => ucfirst(User_getActiveStatus($user->active)), 'registered' => Carbon_HumanDateTime($user->created_at)];
                 });
             }
         }
+        /**
+         * get user detail by code
+         */
         if (request()->has('_user')) {
             if (request('_user')) {
-                $getUser = User::where('code', request('_user'));
-                $getUserHistories = UserLoginHistory::where('code', request('_user'));
-                $data['user'] = count($getUser->get()) ? $getUser->get()->map->userDetailMap()[0] : [];
-                $data['history'] = count($getUserHistories->get()) ? $getUserHistories->get()->map->userLoginHistorySimpleMap() : [];
+                $getUser = User::where('code', request('_user'))->get();
+                $getUserHistories = UserLoginHistory::where('code', request('_user'))->get();
+                $data['user'] = count($getUser) ? $getUser->map->userDetailMap()[0] : [];
+                $data['history'] = count($getUserHistories) ? $getUserHistories->map->userLoginHistorySimpleMap() : [];
             }
         }
         return response()->json(dataResponse($data), 200);
@@ -117,5 +135,31 @@ class AdminMenuController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    # private methods
+    /**
+     * get all user
+     *
+     * @return void
+     */
+    private function getUsers()
+    {
+        return DB::table('users')
+            ->join('user_biodatas', 'users.code', '=', 'user_biodatas.code')
+            ->join('user_statuses', 'users.code', '=', 'user_statuses.code')
+            ->select('name', 'users.code', 'profile_img', 'active', 'status', 'users.created_at')
+            ->where('user_statuses.status', User_setStatus('user'));
+    }
+
+    /**
+     * get user histories
+     *
+     * @param string $userCode
+     * @return void
+     */
+    private function getUserHistory($userCode)
+    {
+        return UserLoginHistory::where('code', $userCode);
     }
 }
