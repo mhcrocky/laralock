b73e830dd4bbc32231ec250d62be1ab9db6c9cb0
es9d <larastar721@gmail.com>
2020-06-08 17:24:49 +0700

    update - image history delete method

--- app/Http/Controllers/APIs/User/UserProfileImageHistController.php
+++ app/Http/Controllers/APIs/User/UserProfileImageHistController.php
@@ -85,6 +85,26 @@ class UserProfileImageHistController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $validator = Validator(request()->all(), [
+            '_histImgCode' => 'required|string|alpha_num',
+            '_method' => 'nullable|string|alpha'
+        ], [
+            '_histImgCode.required' => 'Image code required',
+            '_histImgCode.alpha_num' => 'Image code only contain letters and numbers'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(errorResponse($validator->errors()), 202);
+        }
+        if ((User_getStatus(Auth::user()->userstat->status) == 'admin') && (request('_method') == 'force')) {
+            return response()->json(successResponse('Admin'), 200);
+        } else {
+            $histImg = UserProfileImageHistory::where([['code', Auth::user()->code], ['image_code', request('_histImgCode')]]);
+            if ($histImg->count()) {
+                $histImg->delete();
+                return response()->json(successResponse('Successfully delete history image'), 201);
+            } else {
+                return response()->json(errorResponse('History image not found'), 202);
+            }
+        }
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
             ['status' => User_setStatus('admin'), 'name' => 'My Name is Bachtiar', 'email' => 'bachtiar@mail.com', 'email_verified_at' => '2019-12-07 07:27:47', 'password' => '$2y$10$XsG.kSbJA1g15hcgxp/W6Offn8yLS/igQBBVMzTBBuNvFYt5gb14m', 'code' => '5emMFhIv84in6ccx6gW3xfJ6VqT3vtjxpwvakO6LknK99d0fbr615nDKhJEltks2', 'created_at' => '2019-12-07 07:27:02', 'updated_at' => '2019-12-07 07:27:47', 'active' => User_setActiveStatus('active'), 'profile_img' => '/files/image/profile/users/USR-Q3yszDnYjIkXq7G.jpg'],
             ['status' => User_setStatus('user'), 'name' => 'Sister of Bachtiar', 'email' => 'cashier@mail.com', 'email_verified_at' => '2019-12-07 07:34:40', 'password' => '$2y$10$7hZwzXwDNjgQWdder8b3pe7d3s5s.FAAvaKzYydcY7REoD8/aRAcW', 'code' => 'qdBfFeo7grHXlMIFNCcySEbUCJQyahTw4pysFHXdg5HNAvyL9B94TUo9OkuM7GKp', 'created_at' => '2019-12-07 07:33:29', 'updated_at' => '2019-12-07 07:34:40', 'active' => User_setActiveStatus('active'), 'profile_img' => '/files/image/profile/users/USR-Q3yszDnYjIkXq7G.jpg']
         ];
-
+        // default user
         for ($i = 0; $i < count($user); $i++) {
             User::create([
                 'email' => $user[$i]['email'],
@@ -42,6 +42,37 @@ class UserSeeder extends Seeder
                 'image_url' => $user[$i]['profile_img'],
                 'image_name' => '17689101.jpeg',
                 'image_code' => randString(20)
+            ]);
+        }
+        // add more user
+        for ($i = 0; $i < env('SEED_MORE_USER'); $i++) {
+            $getName = randArray(Seed_getDummyName()) . ' ' . randArray(Seed_getDummyName());
+            $getEmail = strtolower(str_replace(" ", "", $getName)) . mt_rand(1, 99) . '@mail.com';
+            $moreUser = [
+                'code' => User_createNewCode(),
+                'name' => $getName,
+                'email' => $getEmail,
+                'email_verified_at' => Carbon_DBtimeNow(),
+                'password' => '$2y$10$0F3q8CDeDiEhqGlfxWmGPeqLLA7f5AsIgV.MZ6NoouGKaUzV0ZaXq', // @UserTest10
+                'active' => User_setActiveStatus('active'),
+                'status' => User_setStatus('user'),
+                'profile_img' => default_user_image()
+            ];
+            User::create([
+                'email' => $moreUser['email'],
+                'email_verified_at' => $moreUser['email_verified_at'],
+                'password' => $moreUser['password'],
+                'code' => $moreUser['code'],
+                'active' => $moreUser['active']
+            ]);
+            UserBiodata::create([
+                'code' => $moreUser['code'],
+                'name' => $moreUser['name'],
+                'profile_img' => $moreUser['profile_img']
+            ]);
+            UserStatus::create([
+                'code' => $moreUser['code'],
+                'status' => $moreUser['status']
             ]);
         }
     }
